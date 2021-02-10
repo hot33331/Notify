4f24fa90de20bfec4a659558870534eee4d70218
Tobias Hoppenthaler <tobias.hoppenthaler@gmail.com>
2021-02-10 21:08:34 +0100

    first hit

---
+++ Notify.iOS/AppDelegate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+using UserNotifications;
+
+namespace Notify.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+            // Version check
+    if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+                // Request notification permissions from the user
+                UNUserNotificationCenter.Current.RequestAuthorization(UNAuthorizationOptions.Alert, (approved, err) => {
+                    // Handle approval
+                });
+                // Watch for notifications while the app is active
+                UNUserNotificationCenter.Current.Delegate = new UserNotificationCenterDelegate();
+
+            }
+
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
---
+++ Notify/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.LocalNotifications;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Notify
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                CrossLocalNotifications.Current.Show("title", "body");
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                // Feature not supported on device
+            }
+            catch (Exception ex)
+            {
+                // Other error has occurred.
+            }
+        }
+
+        void Button_Clicked2(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                CrossLocalNotifications.Current.Show("title", "body", 101, DateTime.Now.AddSeconds(5));
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                // Feature not supported on device
+            }
+            catch (Exception ex)
+            {
+                // Other error has occurred.
+            }
+        }
+    }
+}
